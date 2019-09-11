32ebc13dc59cbae14dfe55d6041d0457023008b7
khawkriab <khawkriab.dev@outlook.com>
2019-09-12 00:31:54 +0700

    Updates

---
+++ precache-manifest.5f6d2a8e8f5704a3ed87c9e94d62661c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41507633cc87bfa9fdfa9a24e05c6fa",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "4d6340f592df2ca45d17",
+    "url": "/test-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d7c71ecb05e49cafcad9",
+    "url": "/test-deploy/static/js/2.24890e2f.chunk.js"
+  },
+  {
+    "revision": "4d6340f592df2ca45d17",
+    "url": "/test-deploy/static/js/main.9a7d1931.chunk.js"
+  },
+  {
+    "revision": "973a5058b4b772453a49",
+    "url": "/test-deploy/static/js/runtime~main.531c12ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
